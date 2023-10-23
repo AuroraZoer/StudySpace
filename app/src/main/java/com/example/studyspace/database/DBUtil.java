fe40788ca0b364c73dfb20ab5cbce60c5733f6a8
Aurora_Zoe <101802289+AuroraZoer@users.noreply.github.com>
2023-10-23 22:55:02 +0800

    feat: add database tables

--- app/src/main/java/com/example/studyspace/database/DBUtil.java
+++ app/src/main/java/com/example/studyspace/database/DBUtil.java
@@ -6,68 +6,128 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 public class DBUtil extends SQLiteOpenHelper {
-    private Context context;
     private static final String databaseName = "StudySpace.db";
     private static final int databaseVersion = 1;
 
-    public DBUtil(@Nullable Context context) {
+    // User table
+    private static final String TABLE_USER = "User";
+    private static final String USER_COLUMN_ID = "UserID";
+    private static final String USER_COLUMN_USERNAME = "Username";
+    private static final String USER_COLUMN_EMAIL = "Email";
+    private static final String USER_COLUMN_PASSWORD = "Password";
+
+    // StudyRoom table
+    private static final String TABLE_STUDY_ROOM = "StudyRoom";
+    private static final String ROOM_COLUMN_ID = "RoomID";
+    private static final String ROOM_COLUMN_NAME = "Room";
+    private static final String ROOM_COLUMN_BUILDING = "Building";
+    private static final String ROOM_COLUMN_MORNING_AVAILABILITY = "MorningAvailability";
+    private static final String ROOM_COLUMN_AFTERNOON_AVAILABILITY = "AfternoonAvailability";
+    private static final String ROOM_COLUMN_EVENING_AVAILABILITY = "EveningAvailability";
+
+    // StudyTime table
+    private static final String TABLE_STUDY_TIME = "StudyTime";
+    private static final String TIME_COLUMN_ID = "TimeID";
+    private static final String TIME_COLUMN_USER_ID = "UserID";
+    private static final String TIME_COLUMN_ROOM_ID = "RoomID";
+    private static final String TIME_COLUMN_START_TIME = "StartTime";
+    private static final String TIME_COLUMN_END_TIME = "EndTime";
+
+    public DBUtil(Context context) {
         super(context, databaseName, null, databaseVersion);
-        this.context = context;
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE users (username TEXT, " +
-                "password TEXT, " +
-                "email TEXT PRIMARY KEY)");
+    public void onCreate(SQLiteDatabase db) {
+        // Create the User table
+        String createUserTable = "CREATE TABLE " + TABLE_USER + " (" +
+                USER_COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                USER_COLUMN_USERNAME + " TEXT NOT NULL, " +
+                USER_COLUMN_EMAIL + " TEXT NOT NULL UNIQUE, " +
+                USER_COLUMN_PASSWORD + " TEXT NOT NULL);";
+        db.execSQL(createUserTable);
 
+        // Create the StudyRoom table
+        String createStudyRoomTable = "CREATE TABLE " + TABLE_STUDY_ROOM + " (" +
+                ROOM_COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                ROOM_COLUMN_NAME + " INTEGER NOT NULL, " +
+                ROOM_COLUMN_BUILDING + " INTEGER NOT NULL, " +
+                ROOM_COLUMN_MORNING_AVAILABILITY + " INTEGER, " +
+                ROOM_COLUMN_AFTERNOON_AVAILABILITY + " INTEGER, " +
+                ROOM_COLUMN_EVENING_AVAILABILITY + " INTEGER);";
+        db.execSQL(createStudyRoomTable);
+
+        // Create the StudyTime table
+        String createStudyTimeTable = "CREATE TABLE " + TABLE_STUDY_TIME + " (" +
+                TIME_COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                TIME_COLUMN_USER_ID + " INTEGER, " +
+                TIME_COLUMN_ROOM_ID + " INTEGER, " +
+                TIME_COLUMN_START_TIME + " TEXT, " +
+                TIME_COLUMN_END_TIME + " TEXT, " +
+                "FOREIGN KEY(" + TIME_COLUMN_USER_ID + ") REFERENCES " + TABLE_USER + "(" + USER_COLUMN_ID + "), " +
+                "FOREIGN KEY(" + TIME_COLUMN_ROOM_ID + ") REFERENCES " + TABLE_STUDY_ROOM + "(" + ROOM_COLUMN_ID + "));";
+        db.execSQL(createStudyTimeTable);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL(("DROP TABLE IF EXISTS users"));
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(("DROP TABLE IF EXISTS " + TABLE_USER));
 
     }
 
+    /**
+     * Insert a new user into the database
+     *
+     * @param username the username of the user
+     * @param password the password of the user
+     * @param email    the email of the user
+     * @return true if the user was inserted successfully, false otherwise
+     */
     public Boolean insertData(String username, String password, String email) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("username", username);
-        contentValues.put("password", password);
-        contentValues.put("email", email);
-        long result = db.insert("users", null, contentValues);
+        contentValues.put(USER_COLUMN_USERNAME, username);
+        contentValues.put(USER_COLUMN_PASSWORD, password);
+        contentValues.put(USER_COLUMN_EMAIL, email);
+        long result = db.insert(TABLE_USER, null, contentValues);
+        db.close();
 
-        if (result == -1) {
-            return false;
-        } else {
-            return true;
-        }
+        return result != -1;
     }
 
+    /**
+     * Check if the email already exists in the database
+     *
+     * @param email the email to check
+     * @return true if the email exists, false otherwise
+     */
     public Boolean checkEmail(String email) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM users WHERE email = ?", new String[]{email});
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_USER + " WHERE " + USER_COLUMN_EMAIL + " = ?", new String[]{email});
+        boolean emailExists = cursor.getCount() > 0;
+        cursor.close();
+        db.close();
 
-        if (cursor.getCount() > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return emailExists;
 
     }
 
+    /**
+     * Check if the email and password match a user in the database
+     *
+     * @param email    the email to check
+     * @param password the password to check
+     * @return true if the email and password match a user in the database, false otherwise
+     */
     public Boolean checkEmailPassword(String email, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM users WHERE email = ? AND password = ?", new String[]{email, password});
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_USER + " WHERE " + USER_COLUMN_EMAIL + " = ? AND " + USER_COLUMN_PASSWORD + " = ?", new String[]{email, password});
+        boolean loginSuccessful = cursor.getCount() > 0;
+        cursor.close();
+        db.close();
 
-        if (cursor.getCount() > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return loginSuccessful;
     }
 
 }
