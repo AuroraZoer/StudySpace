c7db918b60ce6e793544485c8a74e9fdfb2d65c0
Aurora_Zoe <101802289+AuroraZoer@users.noreply.github.com>
2023-11-18 10:45:09 +0800

    add chart view

--- app/src/main/java/com/example/studyspace/Chart/PieFragment.java
+++ app/src/main/java/com/example/studyspace/Chart/PieFragment.java
@@ -1,66 +1,34 @@
 package com.example.studyspace.Chart;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.charts.Pie;
 import com.example.studyspace.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PieFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PieFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public PieFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PieFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PieFragment newInstance(String param1, String param2) {
-        PieFragment fragment = new PieFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pie, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        AnyChartView anyChartView = view.findViewById(R.id.pie_chart_view);
+
+        Pie pieChart = AnyChart.pie();
+
+        anyChartView.setChart(pieChart);
+
     }
 }
